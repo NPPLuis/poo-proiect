0d0db8832faf39b1585f74f97a15ede0c2bbcdee
NPPLuis <negoitaluis@gmail.com>
2026-05-13 16:47:55 +0300

    Add Battle class with base-pointer storage and copy-and-swap

---
+++ include/battle.h
@@ -0,0 +1,37 @@
+#ifndef OOP_BATTLE_H
+#define OOP_BATTLE_H
+
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "monster.h"
+
+class Knight;
+
+class Battle {
+private:
+    std::string arenaName;
+    std::unique_ptr<Monster> current;
+    std::vector<std::unique_ptr<Monster>> roster;
+
+public:
+    Battle(std::string arena_name, std::vector<std::unique_ptr<Monster>> roster_val);
+    Battle(const Battle& other);
+    Battle& operator=(Battle other);
+    Battle(Battle&&) noexcept = default;
+    ~Battle() = default;
+
+    friend void swap(Battle& a, Battle& b) noexcept;
+
+    void advance();
+    void runRound(Knight& hero) const;
+    [[nodiscard]] int dragonSlayerBonus(int base_damage) const;
+    [[nodiscard]] bool finished() const;
+    [[nodiscard]] std::size_t remaining() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Battle& b);
+};
+
+#endif
